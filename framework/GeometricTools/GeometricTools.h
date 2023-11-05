594e460b1135b07cd797a973f37b6c11892b5f1c
idarlm <idarmy@stud.ntnu.no>
2023-11-05 18:18:26 +0100

    Add GeometricTools.h to framework

---
+++ framework/GeometricTools/GeometricTools.h
@@ -0,0 +1,11 @@
+#include <array>
+
+namespace GeometricTools {
+    constexpr std::array<float, 3*2> UnitTriangle2D = {
+        -0.5f, -0.5f,   // bottom left
+         0.5f, -0.5f,   // bottom right
+         0.0f,  0.5f    // top center
+    };
+
+    
+}
