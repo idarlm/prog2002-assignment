5e706d9113c78239383e0e6b2b24e1f483406e11
idarlm <idarmy@stud.ntnu.no>
2023-11-05 22:57:05 +0100

    Add UnitGridGeometry2D function #5

--- framework/GeometricTools/GeometricTools.h
+++ framework/GeometricTools/GeometricTools.h
@@ -1,4 +1,5 @@
 #include <array>
+#include <vector>
 
 namespace GeometricTools {
     constexpr std::array<float, 3*2> UnitTriangle2D = {
@@ -13,4 +14,28 @@ namespace GeometricTools {
          0.5f,  0.5f,   // top right
         -0.5f,  0.5f    // top left
     };
+
+    /// @brief Makes a unit sized grid of positions.
+    /// @param columns Number of horizontal divisions
+    /// @param rows Number of vertival divisions
+    /// @return Array of values.
+    const float *UnitGridGeometry2D(unsigned columns, unsigned rows) {
+        // vector to store the values
+        std::vector<float> arr{};
+
+        // calculate positions, working out way down one row at a time.
+        for(int i = 0; i < rows+1; i++) {
+            // calculate y position for this row
+            float y = i * (1.f / (float)columns);
+
+            // calculate x positions for this row
+            for (int i = 0; i < columns+1; i++) {
+                float x = i * (1.f / (float)rows);
+                arr.push_back(x);
+                arr.push_back(y);
+            }
+        }
+
+        return arr.data();
+    }
 }
