40620e3dd17c03ae3b28f8f8afa08745d7ee59a9
idarlm <idarmy@stud.ntnu.no>
2023-11-06 02:17:39 +0100

    Fix bugs in grid functions #5

--- framework/GeometricTools/GeometricTools.h
+++ framework/GeometricTools/GeometricTools.h
@@ -19,52 +19,52 @@ namespace GeometricTools {
     /// @param columns Number of horizontal divisions
     /// @param rows Number of vertival divisions
     /// @return Array of values.
-    const float *UnitGridGeometry2D(unsigned columns, unsigned rows) {
+    const std::vector<float> UnitGridGeometry2D(unsigned columns, unsigned rows) {
         // vector to store the position values
-        std::vector<float> pos{};
+        std::vector<float> pos = std::vector<float>();
 
-        // calculate positions, working out way down one row at a time.
+        // calculate positions, working out way up one row at a time.
         for(int i = 0; i < rows+1; i++) {
             // calculate y position for this row
-            float y = i * (1.f / (float)columns);
+            float y = i * (1.f / columns) - 0.5f;
 
             // calculate x positions for this row
             for (int i = 0; i < columns+1; i++) {
-                float x = i * (1.f / (float)rows);
+                float x = i * (1.f / (float)rows) - 0.5f;
                 pos.push_back(x);
                 pos.push_back(y);
             }
         }
 
-        return pos.data();
+        return pos;
     }
 
     /// @brief Calculates indices for UnitGridGeometry2D.
     /// @param columns Number of horizontal divisions
     /// @param rows Number of vertical divisions
     /// @return Array of indices
-    const float *UnitGridTopologyTriangles(unsigned columns, unsigned rows) {
+    const std::vector<unsigned> UnitGridTopologyTriangles(unsigned columns, unsigned rows) {
         /*
-        Indices:
+        Indices for any given row: (i drew this upside down for some reason)
         (n)___(n+1)__(n+2)__..
          |    . |    . |    . |
          | .    | .    | .    |
         (m)___(m+1)__(m+2)__..
         
-        where n = row number * columns.
-        and   m = (row number + 1) * columns
+        where n = row number * (columns + 1)
+        and   m = (row number + 1) * (columns + 1)
         */
 
         // vector to store the indices
-        std::vector<int> indices{};
+        std::vector<unsigned> indices = {};
         
-        // work our way down starting from the top row
+        // work our way up starting from the bottom row
         for (int y = 0; y < rows; y++) {
             //calculate indices for each tile horizontally
             for (int x = 0; x < columns; x++) {
                 // calculate indices
-                auto n = y * columns;
-                auto m = (y + 1) * columns;
+                auto n = x + y * (columns + 1);
+                auto m = x + (y + 1) * (columns + 1);
 
                 // triangle with corner in top left
                 indices.push_back(n);
@@ -72,10 +72,12 @@ namespace GeometricTools {
                 indices.push_back(m);
 
                 // triangle with corner in bottom right
-                indices.push_back(m+1);
                 indices.push_back(m);
+                indices.push_back(m+1);
                 indices.push_back(n+1);
             }
         }
+
+        return indices;
     }
 }
