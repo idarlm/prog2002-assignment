c7f748004c6ebdb34ee30c4dd5dc4cb2182bcf53
idarlm <101576034+idarlm@users.noreply.github.com>
2023-11-17 00:37:37 +0100

    Implement lab2 application

--- labs/lab2/lab2application.h
+++ labs/lab2/lab2application.h
@@ -2,9 +2,23 @@
 #define LAB2APPLICATION_H_
 
 #include <GLFWApplication.h>
+#include <VertexBuffer.h>
+#include <memory>
 
 class Lab2Application : public GLFWApplication {
-	
+public:
+	Lab2Application(std::string name, std::string version);
+
+	// load data
+	unsigned Init() override;
+	// start application loop
+	unsigned Run() const override;
+
+private:
+	VertexBuffer* m_boardVerts;
+	GLuint m_boardArray;
+	GLsizei m_indiceCount;
+	GLuint m_shaderProg;
 };
 
 #endif // LAB2APPLICATION_H_
