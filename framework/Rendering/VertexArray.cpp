3dbb7287cebf4048969c6cb40efbf1a8b13daafa
idarlm <101576034+idarlm@users.noreply.github.com>
2023-11-18 01:16:27 +0100

    Add VertexArray.cpp/.h

---
+++ framework/Rendering/VertexArray.cpp
@@ -0,0 +1,24 @@
+#include "VertexArray.h"
+
+VertexArray::VertexArray() : m_vertexArrayID(0) {}
+VertexArray::~VertexArray() {}
+
+void VertexArray::Bind() const
+{
+
+}
+
+void VertexArray::Unbind() const
+{
+
+}
+
+void VertexArray::AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer)
+{
+
+}
+
+void VertexArray::SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer)
+{
+
+}
