08742de55b023b66226236ce073d1e6056ceb0b5
idarlm <101576034+idarlm@users.noreply.github.com>
2023-11-20 03:26:24 +0100

    Implement RecalculateMatrix

--- framework/Rendering/OrthographicCamera.cpp
+++ framework/Rendering/OrthographicCamera.cpp
@@ -1,16 +1,33 @@
 #include "OrthographicCamera.h"
+#include "glm/gtc/matrix_transform.hpp"
 
 OrthographicCamera::OrthographicCamera(
-    const Frustrum& frustrum = { -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f },
-    const glm::vec3& position = glm::vec3(0.0f), 
-    float rotation = 0.0f
-)
-    : CameraFrustrum(frustrum), Rotation(rotation) 
+    const Frustrum& frustrum,
+    const glm::vec3& position, 
+    float rotation
+) : CameraFrustrum(frustrum), Rotation(rotation) 
 {
-
+    RecalculateMatrix();
 }
 
 void OrthographicCamera::RecalculateMatrix()
 {
+    // update projection matrix
+    ProjectionMatrix = glm::ortho(
+        CameraFrustrum.left,
+        CameraFrustrum.right,
+        CameraFrustrum.bottom,
+        CameraFrustrum.top,
+        CameraFrustrum.near,
+        CameraFrustrum.far
+    );
 
+    // update view matrix
+    ViewMatrix = glm::mat4(1.f);
+    ViewMatrix = glm::translate(ViewMatrix, Position);
+    ViewMatrix = glm::rotate(
+        ViewMatrix, 
+        glm::radians(Rotation), 
+        glm::vec3(0.f, 0.f, 1.f)
+    );
 }
