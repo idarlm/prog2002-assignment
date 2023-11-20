00bf6d88fec835429e898ca63d35f2fe73482bf4
idarlm <101576034+idarlm@users.noreply.github.com>
2023-11-20 02:22:18 +0100

    Add RenderingCommands.h

---
+++ framework/Rendering/RenderingCommands.h
@@ -0,0 +1,41 @@
+#ifndef RENDERINGCOMMANDS_H
+
+#include <glad/glad.h>
+#include <memory>
+#include "VertexArray.h"
+
+namespace RenderCommands
+{
+    inline void Clear(GLuint mode = GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    {
+        glClear(mode);
+    }
+
+    inline void SetPolygonMode(GLenum face, GLenum mode)
+    {
+        glPolygonMode(face, mode);
+    }
+
+    inline void DrawIndex(const std::shared_ptr<VertexArray>& vao, GLenum primitive = GL_TRIANGLES)
+    {
+        glDrawElements(primitive, vao->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+    }
+
+    inline void SetClearColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a) 
+    {
+        glClearColor(r, g, b, a);
+    }
+
+    inline void SetWireframeMode()
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+
+    inline void SetSolidMode()
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+}
+
+
+#endif // !RENDERINGCOMMANDS_H
